f11734fead3d5f5e88feb90897c518571de661d6
Omega Haileyesus <omegahaile@gmail.com>
2018-11-01 16:33:16 -0400

    comments

--- StarNode.java
+++ StarNode.java
@@ -6,4 +6,13 @@ public class StarNode{
     MyNode pocNode = //new MyNode(....);
     knownNodes.add(currentNode, pocNode);
 
+
+    //POC Connect Thread
+
+    //Receiving Messages Thread
+
+    //Calculating RTT Thread
+    
+    //Sending content Thread
+
 }
