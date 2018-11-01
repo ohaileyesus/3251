b045780ac55f1bb674b456a8212614b30d4d2c51
Omega Haileyesus <omegahaile@gmail.com>
2018-11-01 16:26:17 -0400

    structure

---
+++ MyNode.java
@@ -0,0 +1,12 @@
+public class MyNode{
+    String name;
+    String ip;
+    String port;
+
+    public MyNode(String name, String ip, String port) {
+        this.name = name;
+        this.ip = ip;
+        this.port = port;
+    }
+
+}
---
+++ StarNode.java
@@ -0,0 +1,9 @@
+public class StarNode{
+    Set<MyNode> knownNodes = new Set<MyNode>();
+    MyNode hub;
+
+    MyNode currentNode = //new MyNode(....);
+    MyNode pocNode = //new MyNode(....);
+    knownNodes.add(currentNode, pocNode);
+
+}
